771b93a5955e7a99ebade4d30b732737a10944f0
AntonyanGev <138504476+AntonyanGev@users.noreply.github.com>
2023-07-20 23:02:52 +0400

    Create homework2.js

---
+++ homework2.js
@@ -0,0 +1,89 @@
+///1
+function palindrome(str) {  
+    const len = str.length;   
+    for (let i = 0; i < len / 2; i++) {   
+        if (str[i] !== str[len - 1 - i]) {  
+          console.log( 'It is not palindrome'); 
+            break;
+          } else {
+            console.log('palindrom')
+            break;
+        }
+    }
+}
+
+///2--couldn't write
+
+
+
+///3.
+function printNumbers(num) {
+    for (let i = 1; i <= num; i++) {
+        let pattern = '';
+        for (let j = 1; j <= i; j++) {
+            pattern += j;
+        }
+        console.log(pattern);
+    }
+    for (let i = num - 1; i >= 1; i--) {
+        let pattern = '';
+        for (let j = 1; j <= i; j++) {
+            pattern += j;
+        }
+        console.log(pattern);
+    }
+}
+
+///4
+
+function showPattern(param) {
+    for (let i = 1; i <= param; i++) {
+        let spaces = '';
+        for (let j = 1; j < i; j++) {
+            spaces += ' ';
+        }
+        console.log(spaces + '*');
+    }
+}
+
+
+/////5  couldn't write
+
+
+
+
+
+
+
+
+
+
+
+////6
+
+function Pattern (num1, num2) {
+    for (let i = 1; i <= num1; i++) {
+        let pattern = '';
+        for (let j = 1; j <= num2; j++) {
+            pattern += "* ";
+        }
+        console.log (pattern);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
