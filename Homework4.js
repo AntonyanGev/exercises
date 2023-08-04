d49f1b3e402834ea74d942f05ae7bc93e5a07712
AntonyanGev <138504476+AntonyanGev@users.noreply.github.com>
2023-08-05 00:12:11 +0400

    Update Homework4.js

--- Homework4.js
+++ Homework4.js
@@ -1,4 +1,4 @@
-function stringIncludes(str, substr) {
+///1  function stringIncludes(str, substr) {
     for (let i = 0; i <= str.length - substr.length; i++) {
         let found = true;
         
@@ -8,7 +8,6 @@ function stringIncludes(str, substr) {
                 break;
             }
         }
-
         if (found) {
             return true;
         }
@@ -16,3 +15,78 @@ function stringIncludes(str, substr) {
     
     return false;
    }
+
+////2 indexOf
+
+function findIndexOf(str, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+
+///3 replace
+function changer(input, search, replacement) {
+    let result = "";
+    
+    for (let i = 0; i < input.length;) {
+        if (input.substring(i, i + search.length) === search) {
+            result += replacement;
+            i += search.length;
+        } else {
+            result += inputS[i];
+            i++;
+        }
+    }
+
+    return result;
+}
+
+
+///3 replaceAll
+
+function replaceAll(input, str, replacement) {
+    let result = '';
+    let i = 0;
+
+    while (i < input.length) {
+        if (input.substring(i, i + str.length) === str) {
+            result += replacement;
+            i += str.length;
+        } else {
+            result += input[i];
+            i++;
+        }
+    }
+
+    return result;
+}
+
+
+//arr includes
+
+///1. includes
+function includes(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+///2  indexOf
+function findIndexOf(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+//3  Slice ??
